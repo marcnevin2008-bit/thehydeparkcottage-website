591234c1761cc9c22dce8f42b5abbc6b957fd4f2
Marc Nevin <marcnevin2008@MacBook-Pro-2.local>
2025-11-14 16:27:37 -0800

    Add SEO HelmetProvider in main.jsx and Helmet metadata in App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { HelmetProvider } from "react-helmet-async";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HelmetProvider>
     <App />
-  </React.StrictMode>,
-)
+  </HelmetProvider>
+);
